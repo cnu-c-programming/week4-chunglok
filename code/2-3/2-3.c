11ce07c09f2eb80c378d36bbbde15a351f6f2e56
최준화 <choijunhwa072@gmail.com>
2026-03-27 11:41:52 +0900

    Update 2-3.c

---
+++ code/2-3/2-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char a[64] = "Hello";
+    char b[64] = "world";
+    char c[128];
+
+    size_t strlen(const char *a);
+    printf("length of a: %d\n", len);
+}
