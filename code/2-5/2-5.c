461a7d10c79c6718d9251e897922d4d55d652960
최준화 <choijunhwa072@gmail.com>
2026-03-27 11:52:10 +0900

    Update 2-5.c

---
+++ code/2-5/2-5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x = 9.0;
+    double y = 2.5;
+
+    prinf("%.2f\n", sqrt(x));
+    prinf("%.2f\n", pow(x,y));
+    prinf("%.1f\n", ceil(y));
+    prinf("%.1f\n", floor(y));
+    return 0;
+}
