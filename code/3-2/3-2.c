406d097f65b9b4914a77e4c5744e5051be8df146
최준화 <choijunhwa072@gmail.com>
2026-03-29 21:12:21 +0900

    Update 3-2.c

---
+++ code/3-2/3-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void swap_endian(int *x) {
+    char* addr = (char *)x;
+    
+}
+
+int main() {
+    int x = 0x12345678;
+
+    printf("%x\n", x);
+    swap_endian(&x);
+    printf("%x\n", x);
+
+    return 0;
+}
