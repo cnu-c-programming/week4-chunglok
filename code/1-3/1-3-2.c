75e04177293bcf6b9a0f1be3ff354c4388626be6
최준화 <choijunhwa072@gmail.com>
2026-04-01 15:08:05 +0900

    Correct logger_global counter increment behavior
    Fix logger_global function to not increment counter.

--- code/1-3/1-3-2.c
+++ code/1-3/1-3-2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "util.h"
 
-void logger_global(char* message){
+void logger_global(char *message){
     static int counter = 0;
-    printf("log %2d: %s\n", counter++, message);
+    printf("log %2d: %s\n", counter, message);
 }
 
-void task2() {
+void task2(){
     for(int i = 0; i < 4; i++){
         logger_static_inline("task2");
         logger_global("task2");
